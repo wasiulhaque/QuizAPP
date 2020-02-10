02541bc0eeea8dfd43ecbbea9b0224d932dc3bb1
Wasiul Haque <55824191+wasiulhaque@users.noreply.github.com>
2020-02-10 21:25:28 +0600

    Up to Subject Selector Activity

---
+++ app/src/main/java/com/example/quizapp/SubjectSelector.java
@@ -0,0 +1,14 @@
+package com.example.quizapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubjectSelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_selector);
+    }
+}
