d248987ba0e6b9bfcea8eca01bbae086c6ab211f
Jisan129 <jishananam@iut-dhaka.edu>
2020-02-13 03:50:22 +0600

    Select Class

--- app/src/main/java/com/example/quizapp/LogInActivity.java
+++ app/src/main/java/com/example/quizapp/LogInActivity.java
@@ -2,13 +2,25 @@ package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
-
+    Button Login_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+       Login_button=findViewById(R.id.loginId);
+       Login_button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Intent intent=new Intent(LogInActivity.this,Class__Selector.class);
+               startActivity(intent);
+           }
+       });
+
     }
 }
