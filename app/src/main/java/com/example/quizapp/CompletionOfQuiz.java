43a955fc664f8b9c9bb74c9669c7779e10415d2f
Sadman-Saadat <sadmansaadat99@gmail.com>
2020-02-15 19:27:34 +0600

    Up to Quiz completion

---
+++ app/src/main/java/com/example/quizapp/CompletionOfQuiz.java
@@ -0,0 +1,14 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompletionOfQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_completion_of_quiz);
+    }
+}
