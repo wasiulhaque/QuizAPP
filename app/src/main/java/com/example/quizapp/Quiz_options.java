88af61039317f07ef9693b4c49d5119889c215b7
Jisan129 <jishananam@iut-dhaka.edu>
2020-02-15 10:37:57 +0600

    Select Class

---
+++ app/src/main/java/com/example/quizapp/Quiz_options.java
@@ -0,0 +1,14 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Quiz_options extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_options);
+    }
+}
